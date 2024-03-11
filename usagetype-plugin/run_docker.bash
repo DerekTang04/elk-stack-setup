573f3a5dad1bed70a4a51b269623ecda9acb4148
Derek Tang <d2tang@oicr.on.ca>
2024-03-11 18:52:27 -0400

    usagetype builds

---
+++ usagetype-plugin/run_docker.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# exit on attempt to use undeclared variable
+set -o nounset
+# enable error tracing
+set -o errtrace
+# define path to this bash script
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+docker build -t usagetype_env "$SCRIPT_DIR"
+docker run -ti --name build-usagetype usagetype_env
+
+ARTIFACT_DIR="$SCRIPT_DIR"/../elk-stack/setup/ls-config/usagetype-artifacts
+if [[ ! -d "$ARTIFACT_DIR" ]]; then
+    mkdir "$ARTIFACT_DIR"
+fi
+docker cp build-usagetype:/opt/usagetype-plugin/logstash-filter-usage_type-0.0.1.gem "$ARTIFACT_DIR"/
+docker cp build-usagetype:/opt/usagetype-plugin/logstash-filter-usage_type.gemspec "$ARTIFACT_DIR"/
