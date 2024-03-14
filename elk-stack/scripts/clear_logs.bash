07c94cf9408f8b1682c09f1e12c01afe48bd5907
Derek Tang <d2tang@oicr.on.ca>
2024-03-14 15:48:53 -0400

    update sync and clear logs

--- elk-stack/scripts/clear_logs.bash
+++ elk-stack/scripts/clear_logs.bash
@@ -1,34 +1,37 @@
 #!/usr/bin/env bash
 
+# exit on error
+set -e
 # exit on attempt to use undeclared variable
 set -o nounset
 # enable error tracing
 set -o errtrace
 
-WORKING_DIR=/usr/share/filebeat
-REGISTRY_DIR="$WORKING_DIR"/data/registry/filebeat
+INGEST_ROOT=/var/elk-stack-data/filebeat/ingest
+REGISTRY_DIR=/var/elk-stack-data/filebeat/fbdata01/data/registry/filebeat
 
-find "$WORKING_DIR"/ingest_data -type f | while IFS= read -r DATA_FILE; do
+find "$INGEST_ROOT" -type f \( -name "main_*" -o -name "idg_*" -o -name "*.txt" \) | \
+while IFS= read -r DATA_FILE; do
     # get file size, init offset
-    BYTES=$(wc -c ${DATA_FILE} | awk '{print $1}')
+    BYTES="$(wc -c ${DATA_FILE} | awk '{print $1}')"
     OFFSET=0
 
     # try to find offset from log
     ENTRY="$(grep ${DATA_FILE} ${REGISTRY_DIR}/log.json | tail -1)"
-    if [[ $? -eq 0 ]]; then
-        OFFSET=$(echo "$ENTRY" | jq '.v.cursor.offset')
+    if [[ ! "$ENTRY" =~ ^[[:space:]]*$ ]]; then
+        OFFSET="$(echo ${ENTRY} | jq '.v.cursor.offset')"
     fi
 
     # if log is missing offset, try to find from snapshot
     if [[ "$OFFSET" -eq 0 && -f "${REGISTRY_DIR}/active.dat" ]]; then
         SNAPSHOT="$(cat ${REGISTRY_DIR}/active.dat)"
-        ENTRY=$(grep ${DATA_FILE} ${SNAPSHOT})
+        ENTRY="$(grep ${DATA_FILE} ${SNAPSHOT} || true)"
         
-        if [[ $? -eq 0 ]]; then
+        if [[ ! "$ENTRY" =~ ^[[:space:]]*$ ]]; then
             if [[ "${ENTRY: -1}" == "," ]]; then
                 ENTRY="${ENTRY::-1}"
             fi
-            OFFSET=$(echo ${ENTRY} | jq .cursor.offset)
+            OFFSET="$(echo ${ENTRY} | jq '.cursor.offset')"
         fi
     fi
 
--- elk-stack/scripts/sync_apache_logs.bash
+++ elk-stack/scripts/sync_apache_logs.bash
@@ -1,130 +1,101 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# exit on error
 set -e
+# exit on attempt to use undeclared variable
+set -o nounset
+# enable error tracing
+set -o errtrace
 
-#################################################################################
-# USAGE
-#
-# This script works by first checking for files that end in .indexed in the log folder for the specified server. It then marks the corresponding file in s3 to be excluded from a sync to the log folder. 
-# Once the files are in the log folder they are decompressed. They are then given a name based on what LogStash is looking for. Once the files are transfered Logstash will automatically create corresponding indexes
-# In order to reprocess a particular file just remove the corrsponding index file and then rerun the script
-#
-#################################################################################
+LOG_TYPE=""
+INGEST_ROOT=/var/elk-stack-data/filebeat/ingest
+INGEST_DIR=""
+S3_PATH=""
 
-
-# Export the path so when called by cron, the needed binaries (e.g. the aws cli) can be found
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/snap/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin"
-
-echo ""
-echo "starting process"
-date
-current_year=$(date +%Y)
-echo "Processing for year: $current_year"
-echo ""
-
-# where filebeat reads logs from
-elk_log_path="/var/elk-stack/filebeat_ingest_data/"
-echo "Elk log path: $elk_log_path"
-
-
-process_logs() {
-    local domain=${1}
-    echo ""
-    echo ""
-    echo "Processesing logs for: $domain"
-    
-    # where logs are pulled to, munged, and tracked
-    logs_path="/var/elk-stack/logs/$domain/"
-    echo "Logs path: $logs_path"
-
-    if [ $domain = 'reactome.org' ]
-    then
-        s3_path="s3://reactome/private/logs/apache/extended_log/"
-    elif [ $domain = "idg.reactome.org" ]
-    then
-	s3_path="s3://reactome/private/logs/idg-apache/extended_log/"
-    elif [ $domain = "reactomews" ]
-    then
-	s3_path="s3://reactome/private/logs/cpws-server/tomcat7_access_logs/"
-    fi
-
-    sync_cmd="aws s3 sync $s3_path $logs_path"
-    exclude_statements=""
-    while read fullpath
-    do
-       fbname=$(basename "$fullpath" .indexed)
-       [ -z "$fbname" ] || exclude_statements+=" --exclude $fbname"
-    done <<< "$(ls -A1 $logs_path* | egrep "*.indexed$")"
-    sync_cmd_full="$sync_cmd $exclude_statements"
-    echo "Executing Sync Command: $sync_cmd_full"
-    $sync_cmd_full
-
-    echo "Unzipping new gz files"
-    count_gz=`ls -1 $logs_path*.gz 2>/dev/null | wc -l`
-    if [ $count_gz != 0 ]
-    then
-	echo "    - $count_gz files found"
-	if [ $domain = "reactomews" ]
-	then
-	    while read filename
-            do
-		echo "$filename"
-		echo "$logs_path/reactomews/$filename"
-                new_log_filename=${filename::-7}
-		echo "$new_log_filename"
-                tar -I pigz -xvf "$logs_path$filename" -C $logs_path\tmp/
-		cat $logs_path\tmp/* > $logs_path$new_log_filename
-		rm $logs_path\tmp/*
-		rm $logs_path$filename
-            done <<< "$(ls -A1 $logs_path | egrep "*.tar.gz$")"
-	else
-            unpigz $logs_path*.gz
-	fi
+set_vars() {    
+    if [[ "$LOG_TYPE" == "main" ]]; then
+        INGEST_DIR="$INGEST_ROOT"/main
+        S3_PATH=s3://reactome/private/logs/apache/extended_log/
+    elif [[ "$LOG_TYPE" == "idg" ]]; then
+        INGEST_DIR="$INGEST_ROOT"/idg
+        S3_PATH=s3://reactome/private/logs/idg-apache/extended_log/
+    elif [[ "$LOG_TYPE" == "reactomews" ]]; then
+        INGEST_DIR="$INGEST_ROOT"/reactomews
+        S3_PATH=s3://reactome/private/logs/cpws-server/tomcat7_access_logs/
     else
-	echo "    - No file with extension 'gz' found"
+        exit 1
     fi
-    
-    echo ""
-    echo "Moving files to $elk_log_path"
-    echo "$logs_path"
-    
-    while read filename
-    do
-	if [ -z "$filename" ]; then
-	    echo "    - No files to move"
-	else
-            echo "    - Moving $filename"
-	  
-            file_date=${filename: -7}
-	    echo 
-	    echo "filedate: $file_date"
-
-            if [ $domain = "reactome.org" ]
-            then
-		new_filename="extended_log.main.$file_date"
-   	    elif [ $domain = "idg.reactome.org" ]
-	    then
-                new_filename="extended_log.idg.$file_date"
-	    else
-		new_filename="reactomews/cpws_tomcat_7.reactomews.$file_date.txt"
-            fi
-
-	    mv_cmd="mv $logs_path$filename $elk_log_path$new_filename"
-	    echo "$mv_cmd"
-	    $mv_cmd
-	    if [ $domain = "reactomews" ]
-	    then
-		indexed_filename="$filename.tar.gz.indexed"
-            else
-		indexed_filename="$filename.gz.indexed"
-            fi
-	    create_file_cmd="touch $logs_path$indexed_filename"
-	    echo "    - Creating indexed file: $indexed_filename"
-	    $create_file_cmd
-	fi
-    done <<< "$(cd $logs_path && ls -1 --ignore='*.gz.indexed' --ignore='tmp')"
 }
 
+build_expr() {
+    local SYNC_CTRL_FILE="$1"
+    local OPTION="$2"
+    local EXPR=""
 
-process_logs "reactome.org"
-process_logs "idg.reactome.org"
-process_logs "reactomews"
+    while IFS= read -r ARG; do
+        ARG=${ARG%%\#*}
+        if [[ ! "$ARG" =~ ^[[:space:]]*$ ]]; then
+            [[ ! -z "$EXPR" ]] && EXPR+=" "
+            EXPR+="$OPTION $ARG"
+        fi
+    done < "$SYNC_CTRL_FILE"
+
+    echo "$EXPR"
+}
+
+sync_logs() {    
+    local INC_EXPR="$(build_expr ${INGEST_ROOT}/.syncinclude --include)"
+    local EXC_EXPR="$(build_expr ${INGEST_DIR}/.syncexclude --exclude)"
+    
+    echo "Syncing ${S3_PATH}"
+    echo "Include Expression: ${INC_EXPR}"
+    echo "Exclude Expression: ${EXC_EXPR}"
+    aws s3 sync "$S3_PATH" "$INGEST_ROOT"/tmp --exclude "*" $INC_EXPR $EXC_EXPR
+
+    find "$INGEST_ROOT"/tmp -type f | while IFS= read -r FILE; do
+        FBNAME="$(basename -- ${FILE})"
+        echo "$FBNAME" >> "$INGEST_DIR"/.syncexclude
+    done
+    sort -n -o "$INGEST_DIR"/.syncexclude "$INGEST_DIR"/.syncexclude
+}
+
+process_logs() {
+    if [[ "$LOG_TYPE" != "reactomews" ]]; then
+        # set prefix
+        local PREFIX=$([[ "$LOG_TYPE" == "main" ]] && echo "main" || echo "idg")
+
+        find "$INGEST_ROOT"/tmp -type f | while IFS= read -r FILE; do
+            # get inflated file basename
+            FBNAME="$(basename -- ${FILE})"
+            INF_FBNAME="${FBNAME%.*}"
+            # inflate and rename with prefix
+            unpigz "$FILE"
+            mv "$INGEST_ROOT"/tmp/"$INF_FBNAME" "$INGEST_ROOT"/tmp/"$PREFIX"_"$INF_FBNAME"
+        done
+    else
+        # inflate files and remove tar
+        find "$INGEST_ROOT"/tmp -type f | while IFS= read -r FILE; do 
+            tar -xf "$FILE" -C "$INGEST_ROOT"/tmp
+            rm "$FILE"
+        done
+    fi
+
+    # move files to respective ingest_dir if they exist
+    find "$INGEST_ROOT"/tmp -type f \( -name "main_*" -o -name "idg_*" -o -name "*.txt" \) | \
+        grep -q . && \
+        mv "$INGEST_ROOT"/tmp/* "$INGEST_DIR"
+}
+
+run_all() {
+    LOG_TYPE="$1"
+
+    set_vars
+    sync_logs
+    process_logs
+
+    echo; echo
+}
+
+run_all "reactomews"
+run_all "idg"
+run_all "main"
