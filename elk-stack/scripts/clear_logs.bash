9f11840a3266989322a3c41849eb5a2026d7d5d5
Derek Tang <d2tang@oicr.on.ca>
2024-03-15 12:31:24 -0400

    update clear logs

--- elk-stack/scripts/clear_logs.bash
+++ elk-stack/scripts/clear_logs.bash
@@ -8,24 +8,25 @@ set -o nounset
 set -o errtrace
 
 INGEST_ROOT=/var/elk-stack-data/filebeat/ingest
-REGISTRY_DIR=/var/elk-stack-data/filebeat/fbdata01/data/registry/filebeat
+REGISTRY_DIR=/var/elk-stack-data/filebeat/fbdata01/registry/filebeat
 
 find "$INGEST_ROOT" -type f \( -name "main_*" -o -name "idg_*" -o -name "*.txt" \) | \
 while IFS= read -r DATA_FILE; do
-    # get file size, init offset
+    # get file basename and size, init offset
+    DF_BNAME="$(basename -- ${DATA_FILE})"
     BYTES="$(wc -c ${DATA_FILE} | awk '{print $1}')"
     OFFSET=0
 
     # try to find offset from log
-    ENTRY="$(grep ${DATA_FILE} ${REGISTRY_DIR}/log.json | tail -1)"
+    ENTRY="$(grep ${DF_BNAME} ${REGISTRY_DIR}/log.json | tail -1)"
     if [[ ! "$ENTRY" =~ ^[[:space:]]*$ ]]; then
         OFFSET="$(echo ${ENTRY} | jq '.v.cursor.offset')"
     fi
 
     # if log is missing offset, try to find from snapshot
     if [[ "$OFFSET" -eq 0 && -f "${REGISTRY_DIR}/active.dat" ]]; then
-        SNAPSHOT="$(cat ${REGISTRY_DIR}/active.dat)"
-        ENTRY="$(grep ${DATA_FILE} ${SNAPSHOT} || true)"
+        SNAPSHOT="$(ls ${REGISTRY_DIR} | grep -v 'active\|log\|meta')"
+        ENTRY="$(grep ${DF_BNAME} ${REGISTRY_DIR}/${SNAPSHOT} || true)"
         
         if [[ ! "$ENTRY" =~ ^[[:space:]]*$ ]]; then
             if [[ "${ENTRY: -1}" == "," ]]; then
